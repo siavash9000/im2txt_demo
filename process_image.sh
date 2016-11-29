8748aeb8a90ea5a0c5688e711ad059a5136afa80
Siavash Sefidrodi <siavash.sefidrodi@pressrelations.de>
2016-11-29 15:51:52 +0100

    parametrized process_image.sh

--- process_image.sh
+++ process_image.sh
@@ -1,3 +1,10 @@
+if [ $# -eq 0 ]
+then 
+  echo "give path to image is misssing"
+  echo "example: ./process_image.sh imgs/bikes.jpg"
+  exit 3
+fi
+
 # Run inference to generate captions.¬
-im2txt/bazel-bin/im2txt/run_inference --checkpoint_path="/root/im2txt_pretrained/model.ckpt-2000000" --vocab_file="/root/im2txt_pretrained/word_counts.txt" --input_files="/root/imgs/bikes.jpg"
+im2txt/bazel-bin/im2txt/run_inference --checkpoint_path="/root/im2txt_pretrained/model.ckpt-2000000" --vocab_file="/root/im2txt_pretrained/word_counts.txt" --input_files="$1"
 
