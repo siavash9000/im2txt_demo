b32b951ef3b56ca76a406a7542ab00e0e32c438f
Mike Mattozzi <mike.mattozzi@gmail.com>
2017-07-01 22:41:44 -0400

    Adding simple support for supplying a URL rather than a filename

--- process_image.sh
+++ process_image.sh
@@ -1,10 +1,27 @@
 if [ $# -eq 0 ]
 then 
-  echo "give path to image is misssing"
+  echo "path or url to image is missing"
   echo "example: ./process_image.sh imgs/bikes.jpg"
+  echo "example: ./process_image.sh https://github.com/tensorflow/models/raw/master/im2txt/g3doc/COCO_val2014_000000224477.jpg"
   exit 3
 fi
 
-# Run inference to generate captions.¬
-im2txt/bazel-bin/im2txt/run_inference --checkpoint_path="/root/im2txt_pretrained/model.ckpt-2000000" --vocab_file="/root/im2txt_pretrained/word_counts.txt" --input_files="$1"
+INPUTFILE=$1
+echo $INPUTFILE | grep '^https\{0,1\}://'
+if [ $? -eq 0 ]
+then
+  TMPFILE="imgs/$(cat /proc/sys/kernel/random/uuid).jpg"
+  wget -nv -O $TMPFILE "$INPUTFILE"
+  INPUTFILE=$TMPFILE
+fi
 
+echo "Processing $INPUTFILE"
+
+# Run inference to generate captions.¬
+im2txt/bazel-bin/im2txt/run_inference --checkpoint_path="/root/im2txt_pretrained/model.ckpt-2000000" --vocab_file="/root/im2txt_pretrained/word_counts.txt" --input_files="$INPUTFILE"
+
+if [ -v TMPFILE ]
+then
+  echo "Removing tmp file $TMPFILE"
+  rm "$TMPFILE"
+fi
